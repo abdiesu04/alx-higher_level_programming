be61d92a8d1cc16798f611255200e024f9fe0628
root <root@kika.localdomain>
2022-06-29 22:23:18 +0300

    summit

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,71 +1,59 @@
-#!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
 import unittest
-max_integer = __import__('6-max_integer').max_integer
+max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """unittest class for max_integer"""
+    """Unittest class for max_integer"""
     def test_module_docstring(self):
-        """Tests for module docsting"""
-        m = __import__('6-max_integer').__doc__
+        """Test for module docstring"""
+        m = __import__("6-max_integer").__doc__
         self.assertTrue(len(m) > 1)
 
     def test_function_docstring(self):
-        """Tests for funstion docstring"""
-        f = max_integer.__doc__
-        self.assertTrue(len(f) > 1)
+        """Test for function docstring"""
+        m = __import__("6-max_integer").max_integer.__doc__
+        self.assertTrue(len(m) > 1)
 
-    def test_empty_list(self):
-        """Tests for empty list []"""
-        e = []
-        self.assertIsNone(max_integer(e))
+    def test_max_positive_integr(self):
+        """Test for all positive with max in beginning, middle and end"""
+        self.assertEqual(max_integer([34, 6, 9]), 34)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([3, 16, 8]), 16)
 
-    def test_no_args(self):
-        """Tests for no arguments passed to func"""
-        self.assertIsNone(max_integer())
+    def test_max_negative_integer(slef):
+        """Test for all positive with max in beginning, middle and end"""
+        slef.assertEqual(max_integer([-45, -67, -12]), -12)
+        slef.assertEqual(max_integer([-81, -22, -92]), -22)
+        slef.assertEqual(max_integer([-100, -97, -35]), -35)
 
-    def test_one_element(self):
-        """Tests for only one number in the list"""
-        o = [1]
-        self.assertEqual(max_integer(o), 1)
-
-    def test_positive_end(self):
-        """Tests for all positive with max at end"""
-        e = [2, 10, 8, 36, 14, 50]
-        self.assertEqual(max_integer(e), 50)
-
-    def test_positive_middle(self):
-        """Tests for all positive with max in middle"""
-        m = [2, 10, 8, 360, 14, 50]
-        self.assertEqual(max_integer(m), 360)
-
-    def test_positive_beginning(self):
-        """Tests for all positive with max at beginning"""
-        b = [200, 10, 8, 36, 14, 50]
-        self.assertEqual(max_integer(b), 200)
-
-    def test_one_negative(self):
-        """Tests for list with one negative number"""
-        on = [200, 10, 8, -36, 14, 50]
-        self.assertEqual(max_integer(on), 200)
-
-    def test_all_negative(self):
-        """Tests for list with all negative numbers"""
-        n = [-6, -50, -75, -1, -100]
-        self.assertEqual(max_integer(n), -1)
+    def test_max_integer(self):
+        """Test for any integer number"""
+        self.assertEqual(max_integer([10, 0, -2]), 10)
+        self.assertEqual(max_integer([-3, -7, 0, -2]), 0)
 
     def test_none(self):
-        """Tests for passing none as argument"""
+        """Test for empty list []"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_no_argumnet(self):
+        """Test for no argument"""
+        self.assertIsNone(max_integer())
+
+    def test_one_argument(self):
+        """Test one argumnet"""
+        self.assertEqual(max_integer([-1]), -1)
+
+    def test_none(self):
+        """Test for None as argument"""
         with self.assertRaises(TypeError):
             max_integer(None)
 
-    def test_non_int_arg(self):
-        """Tests for a non-int type in list"""
-        string = [1, 2, "Hello", 4, 5]
+    def test_wrong_type(self):
+        """Test for wrong data type"""
+        string = [1, 3, "wrong"]
         with self.assertRaises(TypeError):
             max_integer(string)
+
 
 if __name__ == "__main__":
     unittest.main()
