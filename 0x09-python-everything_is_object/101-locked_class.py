a7a5db1223fef8ce8cc305446afeb31c0272ebf2
root <root@kika.localdomain>
2022-06-29 22:05:08 +0300

    summit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+    101-locked_class: class LockedClass
+"""
+
+
 class LockedClass:
-    """A locked class that only lets the user dynamically create the instance
-    attribute 'first_name'"""
+    """
+        A class that can only have one attribute first_name.
+        Attribute:
+             first_name (str): name
+    """
     __slots__ = ['first_name']
